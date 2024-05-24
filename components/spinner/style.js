6aae3bf2760097d203948be3f55ec901347265c2
unknown <sistemas@TFEQUIPOLP2.Prestalana.local>
2024-05-24 10:49:01 -0600

    Spinner.jsx: styled components separados en style.js

---
+++ components/spinner/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const SpinnerWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const CustomLoader = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: conic-gradient(#0000 10%, #4d869c);
+  mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+  animation: s3 1s infinite linear;
+
+  @keyframes s3 {
+    to {
+      transform: rotate(1turn);
+    }
+`;
