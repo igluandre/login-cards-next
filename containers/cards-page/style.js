8098125522d9f87606311e958056678d073f4d2c
unknown <sistemas@TFEQUIPOLP2.Prestalana.local>
2024-05-24 12:31:52 -0600

    cards-page/styles: Container eliminado

--- containers/cards-page/style.js
+++ containers/cards-page/style.js
@@ -4,13 +4,6 @@ export const Section = styled.section`
   margin: 30px 0px;
 `;
 
-export const Container = styled.div`
-  max-width: 1500px;
-  margin: 0 auto;
-  padding: 0 40px;
-  overflow: auto;
-`;
-
 export const CardWrapper = styled.ul`
   display: flex;
   flex-wrap: wrap;
